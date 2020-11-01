ff0335a3e8d9bddb4e6e8f38ba9c560f2b9f9d0a
oldo <oldo.nicho@gmail.com>
2020-11-01 16:24:51 +1100

    move resume files to separate directory

---
+++ components/Home/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = ({}) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Header;
